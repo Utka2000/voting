b3163a61e0d384dac79cedced0f227a5aa067235
Utka2000 <utkarsh.yashraj@dymensions.io>
2023-06-08 18:37:23 +0530

    Output in console

---
+++ index.js
@@ -0,0 +1,14 @@
+/**Write a JS program to read the age of a candidate and determine whether he is eligible to cast his/her own vote.    
+Test Data: 21   **/
+
+let x=(age)=>{
+    if(age>=18){
+        console.log("Congratulation! You are eligible to cast your vote.  ");
+    }
+    else
+    {
+        console.log("Candidate is not Eligible to Caste the Voter")
+    }
+    
+}
+x(21)
